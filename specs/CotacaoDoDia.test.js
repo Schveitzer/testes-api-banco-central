eb99a02eaaa6d28ef194e9e1f341b5b08336a9ed
Alan Schveitzer <alan.schveitzer@gmail.com>
2020-10-23 19:09:13 -0300

    Adiciona novos testes

--- specs/CotacaoDoDia.test.js
+++ specs/CotacaoDoDia.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-destructuring */
 import { clienteHelper } from '../helpers/Cliente.helper';
 import { StatusCodes } from 'http-status-codes/build/es';
 import { fsHelper } from '../helpers/FileSystem.helper';
@@ -14,11 +15,11 @@ beforeAll(async () => {
     client = await clienteHelper.getClient();
     endpoint = 'CotacaoDolarDia(dataCotacao=@dataCotacao)';
     dataAtual = moment(new Date()).tz('America/Sao_Paulo').format('MM-DD-YYYY'); // Data Atual Ex: 10-25-2020
-    regexPadraoMonetario = /^(\d)+(\.\d{1,4})$/; // Padrão de número monetário Ex: 4.2345
+    regexPadraoMonetario = /^(\d)+(\.\d{1,4})$/; // Padrão de número monetário Ex: 4,2345
 });
 
 describe('Testes para o endpoint de cotações do dia, Endpoint: /CotacaoDolarDia(dataCotacao=@dataCotacao) ', () => {
-    it('Verifica se a cotação para uma data passada não é um valor negativo ', async () => {
+    it('GET - Verifica se a cotação para uma data passada não é um valor negativo ', async () => {
         const params = { '@dataCotacao': `'10-15-2018'`, $format: 'json' };
         const request = await client
             .get(endpoint)
@@ -32,7 +33,7 @@ describe('Testes para o endpoint de cotações do dia, Endpoint: /CotacaoDolarDi
         expect(cotacaoVenda).toBeGreaterThan(0);
     });
 
-    it('Verifica se o valor da cotação é separado por vírgula e se apresenta até quatro casas decimais ', async () => {
+    it('GET - Verifica se o valor da cotação é separado por vírgula e se apresenta até quatro casas decimais ', async () => {
         const params = { '@dataCotacao': `'${dataAtual}'`, $format: 'json' };
         const request = await client
             .get(endpoint)
@@ -48,12 +49,12 @@ describe('Testes para o endpoint de cotações do dia, Endpoint: /CotacaoDolarDi
             expect(cotacaoVenda).toMatch(regexPadraoMonetario);
         } catch (e) {
             throw new Error(
-                `O valor de cotação não correspondeu ao padrão esperado ex: 4.2345, Valores recebidos: Compra: ${cotacaoCompra} | Venda: ${cotacaoVenda}`,
+                `O valor de cotação não correspondeu ao padrão esperado ex: 4,2345, Valores recebidos: Compra: ${cotacaoCompra} | Venda: ${cotacaoVenda} `,
             );
         }
     });
 
-    it('Verifica se a data da cotação recebida está correta para o período consultado ', async () => {
+    it('GET - Verifica se a data da cotação recebida está correta para o período consultado ', async () => {
         const data = '2020-10-15';
         const params = { '@dataCotacao': `'10-15-2020'`, $format: 'json' };
         const request = await client
@@ -66,7 +67,7 @@ describe('Testes para o endpoint de cotações do dia, Endpoint: /CotacaoDolarDi
         expect(dataCotacao).toContain(data);
     });
 
-    it('Valida se os dados retornados estão corretos quando comparados com o arquivo: cotacao_do_dia_10_05_2020.json ', async () => {
+    it('GET - Valida se os dados retornados estão corretos quando comparandos com o arquivo: cotacao_do_dia_10_05_2020.json ', async () => {
         const params = { '@dataCotacao': `'10-05-2020'`, $format: 'json' };
         const request = await client
             .get(endpoint)
@@ -77,7 +78,7 @@ describe('Testes para o endpoint de cotações do dia, Endpoint: /CotacaoDolarDi
         expect(request.body).toEqual(arquivoBase);
     });
 
-    it('Valida o Json Schema verificando se a estrutura de dados do json recebido é o esperado', async () => {
+    it('GET - Valida o Json Schema verificando se a estrutura de dados do json recebido é o esperado', async () => {
         const params = { '@dataCotacao': `'${dataAtual}'`, $format: 'json' };
         const request = await client
             .get(endpoint)
@@ -88,5 +89,40 @@ describe('Testes para o endpoint de cotações do dia, Endpoint: /CotacaoDolarDi
         expect(
             schemaValidatorHelper.validateSchema(schemaBase, request.body),
         ).toBe(true);
+    });
+
+    it('GET - Verifica o retorno quando informado um ano que não existe ', async () => {
+        const params = { '@dataCotacao': `'10-15-3520'`, $format: 'json' };
+        const request = await client
+            .get(endpoint)
+            .query(params)
+            .expect(StatusCodes.OK);
+        const response = request.body.value;
+
+        expect(response.length).toEqual(0);
+    });
+
+    it('GET - Verifica o retorno com o formato text/plain ', async () => {
+        const params = {
+            '@dataCotacao': `'10-15-2020'`,
+            $format: 'text/plain',
+        };
+        const request = await client
+            .get(endpoint)
+            .query(params)
+            .expect(StatusCodes.OK);
+        const response = request.text;
+
+        expect(response).toEqual(
+            'cotacaoCompra,cotacaoVenda,dataHoraCotacao\r\n"5,6166","5,6172",2020-10-15 13:04:30.297\r\n',
+        );
+    });
+
+    it('GET - Verifica se é retornado erro 500 (INTERNAL_SERVER_ERROR) quando informado uma data no formato inválido ', async () => {
+        const params = { '@dataCotacao': `'15-15-2020'`, $format: 'json' };
+        const request = await client
+            .get(endpoint)
+            .query(params)
+            .expect(StatusCodes.INTERNAL_SERVER_ERROR);
     });
 });
